9124190c46dd3eb8043163d5f6e4dfec6c59b3c6
Inesa09 <inesusja@gmail.com>
2018-07-23 19:53:59 +0300

    Movement in 4 sides

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const frame = document.getElementById("game");
 const snake = document.getElementsByClassName("snake")[0];
-let id, translate = 0;
+let id;
+let coord = [0, 0];
 
 
 const getPosition = (element) => {
@@ -26,27 +27,48 @@ const gameOver = () => {
     }
 }
 
-const move = (side, increment) => {
-    snake.style.transform = "translate" + side + "(" + translate + "px)";
-    console.log("frame - " + getPosition(frame) + " snake - " + getPosition(snake));
-    if (increment)
-        translate++;
-    else
-        translate--;
+const increment = (val) => ++val;
+const decrement = (val) => --val;
+
+const move = (side, func) => {
+    snake.style.transform = "translate(" + coord[0] + "px," + coord[1] + "px)";
+    coord[side] = func(coord[side]);
+    // console.log(coord[side]);
+    console.log("x: " + coord[0] + ", y: " + coord[1]);
+    // console.log("frame - " + getPosition(frame) + " snake - " + getPosition(snake));
+    // console.log(snake.style.transform);
+    // if (increment)
+    //     side++;
+    // else
+    //     side--;
     gameOver();
 }
 
 const moveToDirection = (direction) => {
-    let side, increment = false;
+    switch(direction){
+        case("Left"):
+            id = setInterval(move, 5, 0, decrement);
+            break;
+        case("Right"):
+            id = setInterval(move, 5, 0, increment);
+            break;
+        case("Up"):
+            id = setInterval(move, 5, 1, decrement);
+            break;
+        case("Down"):
+            id = setInterval(move, 5, 1, increment);
+            break;
 
-    if (direction === "Left" || direction === "Right")
-        side = "X";
-    else 
-        side = "Y";
-    if (direction === "Right" || direction === "Down")
-        increment = true;
+    }
 
-    id = setInterval(move, 5, side, increment);
+    // if (direction === "Left" || direction === "Right")
+    //     side = x;
+    // else 
+    //     side = y;
+    // if (direction === "Right" || direction === "Down")
+    //     increment = true;
+
+    // id = setInterval(move, 5, side, increment);
 }
 
 
